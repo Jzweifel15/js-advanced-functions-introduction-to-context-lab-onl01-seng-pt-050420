54e4cfb82865c83ab2b03ca139f7b9eeeb9bc614
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-12-01 01:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ function hoursWorkedOnDate(employeeRecord, dateStamp)
   let dateIn = employeeRecord.timeInEvents.find(x => x.date === dateStamp);
   let dateOut = employeeRecord.timeOutEvents.find(x => x.date === dateStamp);
   
-  let timeIn = (dateIn.hour % 12) || 12;
-  let timeOut = (dateOut.hour % 12) || 12;
+  let timeIn = dateIn.hour % 12;
+  let timeOut = dateOut.hour % 12;
   
-  
+  return timeOut - timeIn;
 }
 
